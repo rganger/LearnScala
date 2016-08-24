de795005516b49455741fd281558f875561b61be
Robert Ganger <ganger@cubrc.org>
2016-08-23 20:46:24 -0400

    Added previous tutorial code

--- Basics/build.sbt
+++ Basics/build.sbt
@@ -4,3 +4,5 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
     
+libraryDependencies ++= Seq("org.slf4j" % "slf4j-api" % "1.7.5",
+  "ch.qos.logback" % "logback-classic" % "1.1.7")
---
+++ Basics/src/main/scala/org/cubrc/example/LearningScala.scala
@@ -0,0 +1,13 @@
+package org.cubrc.example
+
+/**
+  * Created by ganger on 5/21/2016.
+  */
+object LearningScala {
+
+  def main(args: Array[String]): Unit = {
+    val basics = new Basics
+    basics.testBasics
+  }
+
+}
